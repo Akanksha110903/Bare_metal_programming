df760e0351d2d78584887a3f6c2f20413ebd4f19
Akanksha <akanksha.bhaisare@atlascopco.com>
2026-03-27 15:14:51 +0530

    uart rx driver , polling method

---
+++ uart_modular_rx/Src/main.c
@@ -0,0 +1,42 @@
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "uart.h"
+
+#define GPIODEN (1U<<3)
+#define PIN12                (1U<<12)
+#define LED_GREEN            PIN12
+void delay(void);
+char key;
+int main(void)
+{
+	RCC->AHB1ENR |= GPIODEN;
+	//set pin 12 as output mode
+	GPIOD->MODER &= ~(3U<<(2*12));
+	GPIOD->MODER |= (1U<<(2*12));
+
+	uart2_rx_tx_init();
+
+
+	while(1)
+	{
+		key = uart2_read();
+
+
+		if(key =='l')
+		{
+			GPIOD->ODR |=  LED_GREEN;
+		}else
+		{
+			GPIOD->ODR &= ~(LED_GREEN);
+		}
+		delay();
+
+
+	}
+}
+void delay(void)
+{
+    for(int i=0;i<50000;i++);
+}
+
+
