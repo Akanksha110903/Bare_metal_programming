cba516c8f7e1ac9e99c62ed6b65891a2776383b5
Akanksha <akanksha.bhaisare@atlascopco.com>
2026-03-28 16:30:54 +0530

    adc driver single conversion

---
+++ adc_single_conversion_driver/Src/main.c
@@ -0,0 +1,28 @@
+#include <stdint.h>
+#include "stm32f4xx.h"
+#include "uart.h"
+#include "adc.h"
+
+//PA1 for adc
+//PD5 FOR TX
+//PD6 FOR RX
+uint32_t sensor_value;
+
+int main(void)
+{
+	uart2_rx_tx_init();
+	PA1_adc_init();
+
+
+	 while(1)
+	 {
+		 start_conversion();
+		 sensor_value = adc_read();
+		 uart2_write(sensor_value);
+
+	 }
+
+}
+
+
+
